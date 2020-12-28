2f3a34734c76920dfb9d8f3c561cb0f20735739b
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-12-28 13:15:25 +0100

    Update package-info.java

--- src/main/java/com/github/tkuenneth/nativeparameterstoreaccess/package-info.java
+++ src/main/java/com/github/tkuenneth/nativeparameterstoreaccess/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package provides access to native parameter stores.
- * Currently the Windows registry and macOS Defaults are supported.
+ * Currently the Windows Registry and the macOS Defaults database are supported.
  *
  * @author Thomas Kuenneth
  */
